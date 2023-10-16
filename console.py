9433de1ae3f42db78f1ec1691a9da653772e63d6
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-10-15 19:04:09 -0700

    update console

--- console.py
+++ console.py
@@ -12,22 +12,6 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
 class HBNBCommand(cmd.Cmd):
     """Representation of the console class"""
     prompt = "(hbnb) "
@@ -71,7 +55,7 @@ class HBNBCommand(cmd.Cmd):
         print()
     def do_create(self, line):
         """Create an instance of BaseModel.Usage: create <model name>"""
-        args = parse(line)
+        args = line.split()
         class_name = args[0]
         if len(args) == 0:
             print("** class name missing **")
@@ -84,7 +68,7 @@ class HBNBCommand(cmd.Cmd):
         """Print the string representation of an instance based on the class name.
         Usage: show <class_name> <instance_id>
         """
-        args = parse(line)
+        args = line.split()
         class_name = args[0]
         instance_id = args[1]
         objects_dict = storage.all()
@@ -104,7 +88,7 @@ class HBNBCommand(cmd.Cmd):
         """Usage: destroy <class> <id> or <class>.destroy(<id>)
         Delete a class instance of a given id.
         """
-        args = parse(line)
+        args = line.split()
         objects_dict = storage.all()
         if len(args) == 0:
             print("** class name missing **")
@@ -120,7 +104,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, line):
         """Prints all string representation of all instances based on class name."""
-        args = parse(line)
+        args = line.split()
         if len(args) > 0 and args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
