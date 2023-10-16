cc7badd3a320fa1f7abc74a87c2de117c08d40a9
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-10-16 12:07:27 -0700

    updsate console

---
+++ console1.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+"""Entry point to the console"""
+import cmd
+import sys
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class HBNBCommand(cmd.Cmd):
+    """Representation of the console class"""
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+    def __init__(self):
+        super().__init__()
+
+    def handle_non_interactive_commands(self):
+        """Handle commands in non-interactive mode"""
+        for line in sys.stdin:
+            self.onecmd(line.strip())
+    def do_EOF(self, line):
+        """Exit the console"""
+        return True
+    def do_quit(self, line):
+        """Exit the console"""
+        return True
+    def help_quit(self):
+        """Print help message for the quit command."""
+        print("Quit command to exit the program")
+        print()
+    def help_create(self):
+        print("Create command to create a new instance of a class and prints its id")
+        print()
+    def help_show(self):
+        print("Show command to print the string representation of an instance based on the class name and id")
+        print()
+    def help_all(self):
+        print("All command to print all string representations of instances based on the class name")
+        print()
+    def help_update(self):
+        print("Update command to update  an instance based on the class name and id by adding or updating attribute")
+        print()
+    def do_create(self, line):
+        """Create an instance of BaseModel.Usage: create <model name>"""
+        args = line.split()
+        class_name = args[0]
+        if len(args) == 0:
+            print("** class name missing **")
+        elif class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(args[0])().id)
+            storage.save()
+    def do_show(self, line):
+        """Print the string representation of an instance based on the class name.
+        Usage: show <class_name> <instance_id>
+        """
+        args = line.split()
+        class_name = args[0]
+        instance_id = args[1]
+        objects_dict = storage.all()
+        key = "{}.{}".format(class_name, instance_id)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif key not in objects:
+            print("** no instance found **")
+        else:
+            print(objects_dict["{}.{}".format(args[0], args[1])])
+    
+    def do_destroy(self, line):
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Delete a class instance of a given id.
+        """
+        args = line.split()
+        objects_dict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in objects_dict.keys():
+            print("** no instance found **")
+        else:
+            del objects_dict["{}.{}".format(args[0], args[1])]
+            storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all instances based on class name."""
+        args = line.split()
+        if len(args) > 0 and args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            instances  = []
+            for obj in storage.all().values():
+                if len(args) > 0 and args[0] == obj.__class__.__name__:
+                    instances.append(obj.__str__())
+                elif len(args) == 0:
+                    instances.append(obj.__str__())
+            print(instances)
+if __name__ == "__main__":
+    if not sys.stdin.isatty():
+        HBNBCommand().handle_non_interactive_commands()
+    else:
+        HBNBCommand().cmdloop()
