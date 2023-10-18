7d674b0b008bf9e19c771342774449231bbd906b
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-10-17 19:26:02 -0700

    updated console

--- models/base_model.py
+++ models/base_model.py
@@ -17,13 +17,14 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        strtimeformat = "%Y-%m-%dT%H:%M:%S.%f"
 
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     if isinstance(value, datetime):
                         value = value.isoformat()
-                    self.__dict__[key] = datetime.fromisoformat(value)
+                    self.__dict__[key] = datetime.strptime(value, strtimeformat)
                 else:
                     self.__dict__[key] = value
         else:
@@ -36,8 +37,8 @@ class BaseModel:
     def to_dict(self):
         """Returns the dictionary representation of object instances"""
         dict_rep = self.__dict__.copy()
-        dict_rep["created_at"] = datetime.today()
-        dict_rep["updated_at"] = datetime.today()
+        dict_rep["created_at"] = self.created_at.isoformat()
+        dict_rep["updated_at"] = self.updated_at.isoformat()
         dict_rep["__class__"] = self.__class__.__name__
         return dict_rep
 
